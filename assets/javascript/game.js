7f3ad66b65f0bc3205653cef4ab26e447cbd551c
cm718 <cm718@icloud.com>
2018-10-24 21:57:44 -0500

    Game is working great. Ready to be turned in. Now refactoring!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,81 +13,63 @@ $(document).ready(function() {
     return Math.floor(Math.random() * (max_value - min_value) + min_value);
   };
 
-  function newGemsAndTarget() {
+  function newGame() {
     // Setting new values for target and gems
-    var targetNum = generateNum(19, 120);
+
+    // Set the targetNum between 19 and 120
+    targetNum = generateNum(19, 120);
     // Set the html for the new target number
     $("#targetNum").text(targetNum);
 
-    // Set give a random number between 1 - 12 to redVal
-    var redVal = generateNum(1, 12);
-    // Click event for red
-    $("#red").on("click", function(){
-      score = score + redVal;
-      $("#score").text(score);
-    });
+    // Give a random number between 1 - 12 to redVal
+    redVal = generateNum(1, 12);
+    $("#red").attr("data-value", redVal);
 
-    // Set give a random number between 1 - 12 to blueVal
-    var blueVal = generateNum(1, 12);
-    // Click event for blueVal
-    $("#blue").on("click", function(){
-      score = score + blueVal;
-      $("#score").text(score);
-    });
+    // Give a random number between 1 - 12 to blueVal
+    blueVal = generateNum(1, 12);
+    $("#blue").attr("data-value", blueVal);
 
-    // Set give a random number between 1 - 12 to yellowVal
-    var yellowVal = generateNum(1, 12);
-    // Click event for yellow
-    $("#yellow").on("click", function(){
-      score = score + yellowVal;
-      $("#score").text(score);
-    });
+    // Give a random number between 1 - 12 to yellowVal
+    yellowVal = generateNum(1, 12);
+    $("#yellow").attr("data-value", yellowVal);
 
-    // Set give a random number between 1 - 12 to greenVal
-    var greenVal = generateNum(1, 12);
-    // Click event for green
-    $("#green").on("click", function(){
-      score = score + greenVal;
-      $("#score").text(score);
-    });
+    // Give a random number between 1 - 12 to greenVal
+    greenVal = generateNum(1, 12);
+    $("#green").attr("data-value", greenVal);
+
     // Console logging for debugging and testing
-    console.log("Target Number is " + targetNum);
-    console.log("Red Number is " + redVal);
-    console.log("Blue Number is " + blueVal);
-    console.log("Yellow Number is " + yellowVal);
-    console.log("Green Number is " + greenVal);
-
+    // console.log("Target Number is " + targetNum);
+    // console.log("Red is " + redVal);
+    // console.log("Blue is " + blueVal);
+    // console.log("Yellow is " + yellowVal);
+    // console.log("Green is " + greenVal);
   };
 
-  function resetGame() {
-    // Call for new gem values and a new target number
-    newGemsAndTarget();
-    // Reset the score to 0
-    score = 0;
-    // Reset the html score to 0
-    $("#score").text("0");
-    // Reset the wins value to 0
-    wins = 0;
-    // Reset the html wins to 0
-    $("#wins").text("0");
-    // Reset the losses value to 0
-    losses = 0;
-    // Reset the losses html to 0
-    $("#losses").text("0");
-  };
-
-  // function gameOver(){
+  function gameOver(){
     // Check if the game is over
     if (score > targetNum){
-      losses = losses + 1;
+      losses++;
       $("#losses").text(losses);
-      newGemsAndTarget();
-    } else if (score = targetNum){
-      wins = wins + 1;
+      alert("BUMMER BRO!");
+      score = 0;
+      newGame();
+      $("#score").text(score);
+    } else if (score == targetNum){
+      wins++;
       $("#wins").text(wins);
-      newGemsAndTarget();
+      alert("MATHEMATICAL!");
+      newGame();
+      score = 0;
+      $("#score").text(score);
     }
-  // }
+  }
 
-  resetGame();
+  $(".crystal").on("click", function(){
+    console.log($(this).attr("data-value"));
+    score = score + parseInt($(this).attr("data-value"));
+    $("#score").text(score);
+    gameOver();
+  });
+
+  newGame();
 });
