6b0dd79ba16b5ff92c21a711d350aca6eb447cb8
cm718 <cm718@icloud.com>
2018-10-24 22:12:30 -0500

    Refactored the newGame function to be able to set the value for all gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
 
   var targetNum = 0;
   var score = 0;
-  var redVal = 0;
-  var blueVal = 0;
-  var yellowVal = 0;
-  var greenVal = 0;
   var wins = 0;
   var losses = 0;
 
@@ -13,37 +9,21 @@ $(document).ready(function() {
     return Math.floor(Math.random() * (max_value - min_value) + min_value);
   };
 
-  function newGame() {
-    // Setting new values for target and gems
+  function setCrystal(crystalType){
+    var crystalVal = generateNum(1, 12);
+    $("#" + crystalType).attr("data-value", crystalVal);
+  }
 
+  function newGame() {
     // Set the targetNum between 19 and 120
     targetNum = generateNum(19, 120);
     // Set the html for the new target number
     $("#targetNum").text(targetNum);
-
-    // Give a random number between 1 - 12 to redVal
-    redVal = generateNum(1, 12);
-    $("#red").attr("data-value", redVal);
-
-    // Give a random number between 1 - 12 to blueVal
-    blueVal = generateNum(1, 12);
-    $("#blue").attr("data-value", blueVal);
-
-    // Give a random number between 1 - 12 to yellowVal
-    yellowVal = generateNum(1, 12);
-    $("#yellow").attr("data-value", yellowVal);
-
-    // Give a random number between 1 - 12 to greenVal
-    greenVal = generateNum(1, 12);
-    $("#green").attr("data-value", greenVal);
-
-    // Console logging for debugging and testing
-    // console.log("Target Number is " + targetNum);
-    // console.log("Red is " + redVal);
-    // console.log("Blue is " + blueVal);
-    // console.log("Yellow is " + yellowVal);
-    // console.log("Green is " + greenVal);
-  };
+    // Run setCrystal for each gem based on it's id
+    $(".crystal").each(function(){
+      setCrystal($(this).attr('id'));
+    })
+  }
 
   function gameOver(){
     // Check if the game is over
